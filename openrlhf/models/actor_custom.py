49a19ed72f00f5fe383e4c6c4a9dc24117c86cb0
Stephen Zhao <szstephenzhao88@gmail.com>
2024-12-13 16:19:38 -0500

    debug shared actor critic

--- openrlhf/models/actor_custom.py
+++ openrlhf/models/actor_custom.py
@@ -535,9 +535,11 @@ class ActorCritic(nn.Module):
         position_ids.masked_fill_(attention_mask == 0, 1)
 
         print("INSPECTION OF FORWARD CALL")
+        print(sequences)
         print(sequences.shape)
         print(position_ids.shape)
         print(attention_mask.shape) # FIgure out where this attention mask is coming from and why it doesn't work with position_ids...
+        1/0
 
         output = self.model(sequences, attention_mask=attention_mask,
                             position_ids=position_ids, output_hidden_states=True)
