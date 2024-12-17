5d8cc4b935101ee842bf306e29c3424c2c4be1df
Stephen Zhao <szstephenzhao88@gmail.com>
2024-12-16 20:52:15 -0500

    debug shared actor critic

--- openrlhf/models/loss.py
+++ openrlhf/models/loss.py
@@ -45,6 +45,11 @@ class PolicyLoss(nn.Module):
         surr1 = ratio * advantages
         surr2 = ratio.clamp(1 - self.clip_eps, 1 + self.clip_eps) * advantages
         loss = -torch.min(surr1, surr2)
+        print("RATIO")
+        print(ratio)
+        print("ADVANTAGES")
+        print(advantages)
+
         print("ACTOR LOSS DETAILS")
         print(surr1)
         print(surr2)
