e1cc89e399385d04a0716c23ef5454eda2a06e7c
Stephen Zhao <szstephenzhao88@gmail.com>
2025-02-14 14:39:27 -0500

    save work, try CTL on (modulated) policy

--- openrlhf/trainer/ppo_trainer.py
+++ openrlhf/trainer/ppo_trainer.py
@@ -937,8 +937,8 @@ class PPOTrainer(ABC):
                 experience.attention_mask)
             final_reward = self.experience_maker.compute_reward_no_kl(experience.sequences, experience.attention_mask)
             log_psi = self.experience_maker.actor(experience.sequences, num_actions, experience.attention_mask, return_only_modulation=True)
-            print(log_psi)
-            1/0
+            # print(log_psi)
+            # 1/0
 
             actor_loss = self.actor_loss_fn(
                 log_psi,
