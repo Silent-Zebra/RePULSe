051b6c6d7b3f2be8eadbedf5f9ffce4278ac4c86
Stephen Zhao <szstephenzhao88@gmail.com>
2025-02-16 02:35:14 -0500

    change saving

--- openrlhf/cli/train_ppo.py
+++ openrlhf/cli/train_ppo.py
@@ -430,9 +430,14 @@ def train(args):
     if args.bc_coef > 0:
         lr_str += f"_bc{args.bc_coef}"
 
+    if args.no_critic:
+        critic_loss_str = ""
+    else:
+        critic_loss_str = f"criticloss{args.critic_loss_type}"
+
     if args.critic_loss_type == "mixed_ctl_mse":
         lr_str += f"_alpha{args.alpha}"
-    save_str = f"{args.save_info_path}/f_q_g_q_iwae_bounds_OpenRLHF_PPO_PPOepochs{args.max_epochs}_{eval_str}_lrschedule{args.lr_scheduler}_{lr_str}_criticloss{args.critic_loss_type}_adambetas{args.adam_betas[0]}_{args.adam_betas[1]}_{extra_str}_seed{args.seed}"
+    save_str = f"{args.save_info_path}/f_q_g_q_iwae_bounds_OpenRLHF_{args.actor_loss_type}_epochs{args.max_epochs}_{eval_str}_lrschedule{args.lr_scheduler}_{lr_str}_{critic_loss_str}_adambetas{args.adam_betas[0]}_{args.adam_betas[1]}_{extra_str}_seed{args.seed}"
 
     torch.save(target_to_save, save_str)
 
