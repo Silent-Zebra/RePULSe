91ec9472b4745934d8b55bba3b784539eb28c0bf
Stephen Zhao <szstephenzhao88@gmail.com>
2025-03-26 02:06:23 -0400

    save work (try on neg beta now)

--- openrlhf/models/loss.py
+++ openrlhf/models/loss.py
@@ -243,7 +243,7 @@ class CTLLoss(nn.Module):
         # print("After mask")
         # print(curr_log_probs.sum(-1))
 
-        if action_mask.shape[-1] > 50: # Where EOS may start to come into play.
+        if action_mask.shape[-1] > 100: # Where EOS may start to come into play.
             raise Exception("CHECK THE EOS AND PADDING AND ACTION MASK CAREFULLY, ENSURE IT WORKS AS EXPECTED. Should work, but just confirm and test")
 
         if reduce_mean_per_prompt:
@@ -451,7 +451,7 @@ class SIXOLoss(nn.Module):
         # print("After mask")
         # print(curr_log_probs.sum(-1))
 
-        if action_mask.shape[-1] > 50:  # Where EOS may start to come into play.
+        if action_mask.shape[-1] > 100:  # Where EOS may start to come into play.
             raise Exception("CHECK THE EOS AND PADDING AND ACTION MASK CAREFULLY, ENSURE IT WORKS AS EXPECTED. Should work, but just confirm and test")
 
 
@@ -653,7 +653,7 @@ class DPGLoss(nn.Module):
         values *= action_mask # This should also be redundant since the masked mean at the end should take care of the values; values (log_psi) should be 0 after the final masked mean and have 0 gradient there for tokens after EOS
         # But I'm leaving the above just to be safe; TODO later can test to ensure this is the case.
 
-        if action_mask.shape[-1] > 50: # Where EOS may start to come into play.
+        if action_mask.shape[-1] > 100: # Where EOS may start to come into play.
             raise Exception("CHECK THE EOS AND PADDING AND ACTION MASK CAREFULLY, ENSURE IT WORKS AS EXPECTED. Should work, but just confirm and test")
 
         if reduce_mean_per_prompt:
