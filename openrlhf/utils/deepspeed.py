5bf9cc7b4e6bd7763abddd3974b67e1c6fb1bcfb
Stephen Zhao <szstephenzhao88@gmail.com>
2025-03-28 20:01:21 -0400

    try track gradient variance and expectations

--- openrlhf/utils/deepspeed.py
+++ openrlhf/utils/deepspeed.py
@@ -128,9 +128,8 @@ class DeepspeedStrategy(ABC):
     def backward(self, loss: torch.Tensor, model: nn.Module, optimizer: optim.Optimizer, **kwargs) -> None:
         if isinstance(model, Actor):
             model = model.model
-        model.backward(loss)
-
-        model.allreduce_gradients()
+        # model.backward(loss)
+        loss.backward()
         for name, param in model.named_parameters():
             print(name)
             print(param.requires_grad)
