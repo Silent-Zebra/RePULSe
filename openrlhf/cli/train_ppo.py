cd1aee7dd54777c86adbd0f751eafd3e2c921380
Stephen Zhao <szstephenzhao88@gmail.com>
2025-04-07 02:39:01 -0400

    aggregate more data

--- openrlhf/cli/train_ppo.py
+++ openrlhf/cli/train_ppo.py
@@ -491,6 +491,8 @@ def train(args):
         from functools import partial
 
         for i in range(len(neg_data) // args.train_batch_size + 1):
+            print(f"BATCH {i}, flush=True")
+
             batch = neg_data[i * args.train_batch_size : (i + 1) * args.train_batch_size]
             # print("BATCH")
             # print(batch)
