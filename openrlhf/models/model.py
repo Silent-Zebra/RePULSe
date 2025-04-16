adac38f9d513ff362013741957f1942aa72a9891
Stephen Zhao <szstephenzhao88@gmail.com>
2025-04-16 19:15:15 -0400

    bug fix

--- openrlhf/models/model.py
+++ openrlhf/models/model.py
@@ -387,6 +387,9 @@ def _get_reward_model_custom(
 
                     # print("inputs to rm 2")
                     # print(inputs)
+                    inputs = {key: value.to(device) for
+                              key, value in
+                              inputs.items()}
 
                     with torch.no_grad():
                         r = self.rm(**inputs).logits.squeeze(-1).detach()
