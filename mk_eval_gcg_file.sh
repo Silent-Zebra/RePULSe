e3fac6162a05371131ef4947965b3cedbf214640
Stephen Zhao <stephen.zhao@mail.utoronto.ca>
2025-09-05 02:21:07 -0400

    save work

--- mk_eval_gcg_file.sh
+++ mk_eval_gcg_file.sh
@@ -1,7 +1,10 @@
 STEPS=$1
 CKPT_PATH=$2
 CKPT_ABBREV="${CKPT_PATH##*/}"
-
+MODEL=$3
+RM=$4
+THRESHOLD=$5
+DATA_CSV=$6
 
 # Get current date in required format
 CURRENT_DATE=$(date +%Y-%m-%d-%H-%M)
@@ -14,29 +17,32 @@ OUTPUT_FILE="result_${CURRENT_DATE}_eval_gcg${STEPS}_${CKPT_ABBREV}.txt"
 cat > "$SBATCH_FILE" << EOL
 #!/bin/bash
 #SBATCH -J s1_$(($RANDOM % 100000))
-#SBATCH --ntasks=1
-#SBATCH --mem=64G
-#SBATCH -c 4
+#SBATCH --mem=48G
 #SBATCH --time=4:00:00
-#SBATCH --partition=a40
-#SBATCH --qos=m3
 #SBATCH --export=ALL
 #SBATCH --output=$OUTPUT_FILE
-#SBATCH --gres=gpu:1
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=1
 cd ~
-ln -s /usr/bin/gcc-10 .local/bin/gcc
-ln -s /usr/bin/g++-10 .local/bin/g++
 export PATH=\$HOME/.local/bin/:\$PATH
-cd ~/OpenRLHF
-source newenv/bin/activate
-module load cuda-12.3
+cd ~/projects/aip-rgrosse/zhaostep/OpenRLHF
+module load StdEnv/2023  gcc/12.3  openmpi/4.1.5
+module load cuda/12.6
+module load scipy-stack/2024a
+module load gcc arrow/18.1.0
+module load opencv/4.12.0
+module load rust
+source ENV/bin/activate
+source ~/.hf_token
 deepspeed --master_port $(($RANDOM % 1000 + 3000))1 --module openrlhf.cli.evaluate_gcg_sz \
-    --pretrain "HuggingFaceTB/SmolLM-135M-Instruct" \
+    --pretrain "${MODEL}" \
     --ckpt_path ${CKPT_PATH} \
-    --file_path data/harmful_behaviors_custom.csv \
+    --file_path data/${DATA_CSV} \
     --atk_success_criteria reward \
-    --reward_pretrain "OpenAssistant/reward-model-deberta-v3-large-v2" \
-    --reward_threshold -4.0 \
+    --reward_pretrain "${RM}" \
+    --reward_threshold ${THRESHOLD} \
     --rm_exclude_adv_suffix \
     --bf16 \
     --zero_stage 2 \
