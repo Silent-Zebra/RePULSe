cc1b8378471ff1f1907fee00350f272a55c76f2d
Stephen Zhao <szstephenzhao88@gmail.com>
2025-09-15 22:10:57 -0400

    Try separate_reweighting_beta as well

--- openrlhf/utils/utils.py
+++ openrlhf/utils/utils.py
@@ -225,7 +225,11 @@ def get_info_name_str(args):
     if args.anneal_target_dist_beta:
         start_beta_str = f"_start{args.start_target_dist_beta}"
 
-    info_name_str = f"{rm_type_str}_{pretrain_str}_{reward_pretrain_str}_{prompt_data_str}_len{args.generate_max_len}_kl{args.init_kl_coef}{start_beta_str}_beta{args.target_dist_beta}{harmlessness_train_str}{rew_trans_str}_{args.parameterization}_{args.actor_loss_type}_epo{args.max_epochs}_epi{n_episodes}{eval_str}_sch{args.lr_scheduler}_{lr_str}{critic_loss_str}{adam_betas_str}_{args.parameterization}{init_head_base_str}{sddiv_str}_s{args.seed}"
+    sep_beta_str = ""
+    if args.separate_reweighting_beta is not None:
+        sep_beta_str = f"_sepb{args.separate_reweighting_beta}"
+
+    info_name_str = f"{rm_type_str}_{pretrain_str}_{reward_pretrain_str}_{prompt_data_str}_len{args.generate_max_len}_kl{args.init_kl_coef}{start_beta_str}_beta{args.target_dist_beta}{sep_beta_str}{harmlessness_train_str}{rew_trans_str}_{args.parameterization}_{args.actor_loss_type}_epo{args.max_epochs}_epi{n_episodes}{eval_str}_sch{args.lr_scheduler}_{lr_str}{critic_loss_str}{adam_betas_str}_{args.parameterization}{init_head_base_str}{sddiv_str}_s{args.seed}"
 
     return info_name_str
 
