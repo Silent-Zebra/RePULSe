b8aa6cc8ff9c5017a2f234cfb5fdfddb5ca5d74b
Stephen Zhao <stephen.zhao@mail.utoronto.ca>
2025-09-22 22:46:07 -0400

    save

--- mk_sb_file_seed1_dcs.sh
+++ mk_sb_file_seed1_dcs.sh
@@ -13,11 +13,11 @@ PARAMS=$(echo "$COMMAND" | awk '
     # Initialize empty variables
     micro_train = train = micro_rollout = rollout = ""
     max_epochs = num_episodes = num_episodes_h = gen_max_len = actor_lr = critic_lr = baseactor_lr = ""
-    target_beta = lr_sched = actor_loss = kl = do_harmlessness = ""
+    target_beta = lr_sched = actor_loss = kl = do_harmlessness = rta = rtb = startb = starta = sepb = uniw = ""
     custom_prompt = prompt_data = parameterization = adam_beta2 = rm_type = dup_rollout = pretrain = reward_pretrain = init_head_from_base = ""
     sd_divider = harmloss = harmlossreinbaseline = hlrbval = ""
     save_negdata_threshold = threshold = alpha = only_eval_neg = use_base_as_proposal = ""
-
+    
     # Scan through all matches in the string
     for(i=1; i<=NF; i++) {
         if($i == "--micro_train_batch_size") micro_train = $(i+1)
@@ -45,16 +45,16 @@ PARAMS=$(echo "$COMMAND" | awk '
         if($i == "--pretrain") {
             abbrev = ""
             n = split(gensub(".*/", "", "g", $(i+1)), arr, "-")
-            for (i = 1; i <= n; i++) {
-                abbrev = abbrev substr(arr[i], 1, 2)  # Append first 2 characters of each word
+            for (j = 1; j <= n; j++) {
+                abbrev = abbrev substr(arr[j], 1, 2)  # Append first 2 characters of each word
             }
             pretrain = abbrev
         }
         if($i == "--reward_pretrain") {
             abbrev = ""
             n = split(gensub(".*/", "", "g", $(i+1)), arr, "-")
-            for (i = 1; i <= n; i++) {
-                abbrev = abbrev substr(arr[i], 1, 2)  # Append first 2 characters of each word
+            for (j = 1; j <= n; j++) {
+                abbrev = abbrev substr(arr[j], 1, 2)  # Append first 2 characters of each word
             }
             reward_pretrain = abbrev
         }
@@ -69,22 +69,26 @@ PARAMS=$(echo "$COMMAND" | awk '
         if($i == "--only_evaluate_on_neg_data") only_eval_neg = "_onlyevalneg"
         if($i == "--do_harmlessness_training") do_harmlessness = 1
         if($i == "--use_base_as_proposal") use_base_as_proposal = "_baseprop"
+	if($i == "--rew_trans_alpha") rta = "_rta"$(i+1)
+	if($i == "--rew_trans_beta") rtb = "_rtb"$(i+1)
+	if($i == "--start_target_dist_beta") startb = "_start"$(i+1)
+	if($i == "--start_alpha") starta = "_start"$(i+1)
+	if($i == "--separate_reweighting_beta") sepb = "_sepb"$(i+1)
+	if($i == "--uniform_reweight") uniw = "_uniw"
     }
     # Use num_episodes_h if do_harmlessness is set
     episodes_to_use = do_harmlessness ? num_episodes_h : num_episodes
-
+    
         print micro_train "|" train "|" micro_rollout "|" rollout "|" max_epochs "|" episodes_to_use "|" \
               gen_max_len "|" actor_lr "|" critic_lr "|" baseactor_lr "|" target_beta "|" save_negdata_threshold "|" threshold "|" lr_sched "|" \
               actor_loss "|" custom_prompt "|" parameterization "|" adam_beta2 "|" rm_type "|" dup_rollout "|" pretrain "|" \
-              reward_pretrain "|" prompt_data "|" init_head_from_base "|" sd_divider "|" harmloss "|" harmlossreinbaseline "|" hlrbval "|" alpha "|" kl "|" only_eval_neg "|" use_base_as_proposal \
-
+              reward_pretrain "|" prompt_data "|" init_head_from_base "|" sd_divider "|" harmloss "|" harmlossreinbaseline "|" hlrbval "|" alpha "|" kl "|" only_eval_neg "|" use_base_as_proposal "|" rta "|" rtb "|" startb "|" starta "|" sepb "|" uniw  \
 }')
 
 # Read using the special delimiter
 IFS='|' read MICRO_TRAIN TRAIN MICRO_ROLLOUT ROLLOUT MAX_EPOCHS NUM_EPISODES GEN_MAX_LEN \
     ACTOR_LR CRITIC_LR BASEACTOR_LR TARGET_BETA SAVE_NEGDATA_THRESH THRESH LR_SCHED ACTOR_LOSS CUSTOM_PROMPT PARAMETERIZATION ADAM_BETA2 RM_TYPE DUP_ROLLOUT PRETRAIN REWARD_PRETRAIN PROMPT_DATA \
-    INITHEADBASE SD_DIVIDER HARMLOSS HARMLOSSREINBASELINE HLRBVAL ALPHA KL ONLY_EVAL_NEG BASE_PROP <<< "$PARAMS"
-
+    INITHEADBASE SD_DIVIDER HARMLOSS HARMLOSSREINBASELINE HLRBVAL ALPHA KL ONLY_EVAL_NEG BASE_PROP RTA RTB STARTB STARTA SEPB UNIW <<< "$PARAMS"
 
 # echo $PRETRAIN
 # PRETRAIN="${PRETRAIN%%/*}"
@@ -95,10 +99,9 @@ IFS='|' read MICRO_TRAIN TRAIN MICRO_ROLLOUT ROLLOUT MAX_EPOCHS NUM_EPISODES GEN
 CURRENT_DATE=$(date +%Y-%m-%d-%H-%M)
 
 # Generate output filename
-PATTERN="${CURRENT_DATE}${ONLY_EVAL_NEG}_${PRETRAIN}_${REWARD_PRETRAIN}_${PROMPT_DATA}_${RM_TYPE}${BASE_PROP}${THRESH}${TARGET_BETA}${KL}_len${GEN_MAX_LEN}_${PARAMETERIZATION}${INITHEADBASE}${SD_DIVIDER}_batch${MICRO_TRAIN}_${TRAIN}_${MICRO_ROLLOUT}_${ROLLOUT}${DUP_ROLLOUT}_epo${MAX_EPOCHS}_epi${NUM_EPISODES}${HARMLOSS}${HARMLOSSREINBASELINE}${HLRBVAL}${ALPHA}${BASEACTOR_LR}_${ACTOR_LOSS}_alr${ACTOR_LR}_clr${CRITIC_LR}_${LR_SCHED}${CUSTOM_PROMPT}${SAVE_NEGDATA_THRESH}"
+PATTERN="${CURRENT_DATE}${ONLY_EVAL_NEG}_${PRETRAIN}_${REWARD_PRETRAIN}_${PROMPT_DATA}_${RM_TYPE}${BASE_PROP}${THRESH}${STARTB}${TARGET_BETA}${SEPB}${UNIW}${KL}_len${GEN_MAX_LEN}_${PARAMETERIZATION}${INITHEADBASE}${SD_DIVIDER}_batch${MICRO_TRAIN}_${TRAIN}_${MICRO_ROLLOUT}_${ROLLOUT}${DUP_ROLLOUT}_epo${MAX_EPOCHS}_epi${NUM_EPISODES}${HARMLOSS}${HARMLOSSREINBASELINE}${HLRBVAL}${STARTA}${ALPHA}${RTA}${RTB}${BASEACTOR_LR}_${ACTOR_LOSS}_alr${ACTOR_LR}_clr${CRITIC_LR}_${LR_SCHED}${CUSTOM_PROMPT}${SAVE_NEGDATA_THRESH}"
 SBATCH_FILE="sbatch_${PATTERN}"
 OUTPUT_FILE="result_${PATTERN}_s1.txt"
-
 
 # Create the sbatch file
 cat > "$SBATCH_FILE" << EOL
@@ -127,6 +130,7 @@ export LD_LIBRARY_PATH=\$CUDA_HOME/lib64:\$LD_LIBRARY_PATH
 export MAX_JOBS=1
 deepspeed --master_port $(($RANDOM % 1000 + 3000))1 $COMMAND
 EOL
+
 
 # Make the sbatch file executable
 chmod +x "$SBATCH_FILE"
