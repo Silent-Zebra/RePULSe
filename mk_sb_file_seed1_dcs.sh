81d7b9c408ac8093ef61192f5e97722653464507
Stephen Zhao <szstephenzhao88@gmail.com>
2026-01-07 17:26:06 -0500

    remove unused arg

--- mk_sb_file_seed1_dcs.sh
+++ mk_sb_file_seed1_dcs.sh
@@ -17,7 +17,7 @@ PARAMS=$(echo "$COMMAND" | awk '
     custom_prompt = prompt_data = parameterization = adam_beta2 = rm_type = dup_rollout = pretrain = reward_pretrain = init_head_from_base = ""
     sd_divider = harmloss = harmlossreinbaseline = hlrbval = ""
     save_negdata_threshold = threshold = alpha = only_eval_neg = use_base_as_proposal = ""
-    exploration_bonus_sa = exploration_bonus_ba = bonus_alpha = coin_flip_dim = coin_flip_lr = coin_flip_batch = analytic_batch = ""
+    exploration_bonus_sa = exploration_bonus_ba = bonus_alpha = coin_flip_dim = coin_flip_lr = analytic_batch = ""
     
     # Scan through all matches in the string
     for(i=1; i<=NF; i++) {
@@ -82,7 +82,6 @@ PARAMS=$(echo "$COMMAND" | awk '
 	if($i == "--bonus_alpha") bonus_alpha = "_a"$(i+1)
 	if($i == "--coin_flip_dim") coin_flip_dim = "_cfd"$(i+1)
 	if($i == "--coin_flip_lr") coin_flip_lr = "_cflr"$(i+1)
-	if($i == "--coin_flip_batch_size") coin_flip_batch = "_cfb"$(i+1)
     }
     # Use num_episodes_h if do_harmlessness is set
     episodes_to_use = do_harmlessness ? num_episodes_h : num_episodes
@@ -93,13 +92,13 @@ PARAMS=$(echo "$COMMAND" | awk '
         print micro_train "|" train "|" micro_rollout "|" rollout "|" max_epochs "|" episodes_to_use "|" \
               gen_max_len "|" actor_lr "|" critic_lr "|" baseactor_lr "|" target_beta "|" save_negdata_threshold "|" threshold "|" lr_sched "|" \
               actor_loss "|" custom_prompt "|" parameterization "|" adam_beta2 "|" rm_type "|" dup_rollout "|" pretrain "|" \
-              reward_pretrain "|" prompt_data "|" init_head_from_base "|" sd_divider "|" harmloss "|" harmlossreinbaseline "|" hlrbval "|" alpha "|" kl "|" only_eval_neg "|" use_base_as_proposal "|" rta "|" rtb "|" startb "|" starta "|" sepb "|" uniw "|" exploration_bonus_sa "|" exploration_bonus_ba "|" bonus_alpha "|" coin_flip_dim "|" coin_flip_lr "|" coin_flip_batch "|" analytic_batch  \
+              reward_pretrain "|" prompt_data "|" init_head_from_base "|" sd_divider "|" harmloss "|" harmlossreinbaseline "|" hlrbval "|" alpha "|" kl "|" only_eval_neg "|" use_base_as_proposal "|" rta "|" rtb "|" startb "|" starta "|" sepb "|" uniw "|" exploration_bonus_sa "|" exploration_bonus_ba "|" bonus_alpha "|" coin_flip_dim "|" coin_flip_lr "|" analytic_batch  \
 }')
 
 # Read using the special delimiter
 IFS='|' read MICRO_TRAIN TRAIN MICRO_ROLLOUT ROLLOUT MAX_EPOCHS NUM_EPISODES GEN_MAX_LEN \
     ACTOR_LR CRITIC_LR BASEACTOR_LR TARGET_BETA SAVE_NEGDATA_THRESH THRESH LR_SCHED ACTOR_LOSS CUSTOM_PROMPT PARAMETERIZATION ADAM_BETA2 RM_TYPE DUP_ROLLOUT PRETRAIN REWARD_PRETRAIN PROMPT_DATA \
-    INITHEADBASE SD_DIVIDER HARMLOSS HARMLOSSREINBASELINE HLRBVAL ALPHA KL ONLY_EVAL_NEG BASE_PROP RTA RTB STARTB STARTA SEPB UNIW EXPLORATION_BONUS_SA EXPLORATION_BONUS_BA BONUS_ALPHA COIN_FLIP_DIM COIN_FLIP_LR COIN_FLIP_BATCH ANALYTIC_BATCH <<< "$PARAMS"
+    INITHEADBASE SD_DIVIDER HARMLOSS HARMLOSSREINBASELINE HLRBVAL ALPHA KL ONLY_EVAL_NEG BASE_PROP RTA RTB STARTB STARTA SEPB UNIW EXPLORATION_BONUS_SA EXPLORATION_BONUS_BA BONUS_ALPHA COIN_FLIP_DIM COIN_FLIP_LR ANALYTIC_BATCH <<< "$PARAMS"
 
 # echo $PRETRAIN
 # PRETRAIN="${PRETRAIN%%/*}"
@@ -110,7 +109,7 @@ IFS='|' read MICRO_TRAIN TRAIN MICRO_ROLLOUT ROLLOUT MAX_EPOCHS NUM_EPISODES GEN
 CURRENT_DATE=$(date +%Y-%m-%d-%H-%M)
 
 # Generate output filename
-PATTERN="${CURRENT_DATE}${ONLY_EVAL_NEG}_${PRETRAIN}_${REWARD_PRETRAIN}_${PROMPT_DATA}_${RM_TYPE}${BASE_PROP}${THRESH}${STARTB}${TARGET_BETA}${SEPB}${UNIW}${KL}_len${GEN_MAX_LEN}_${PARAMETERIZATION}${INITHEADBASE}${SD_DIVIDER}_batch${MICRO_TRAIN}_${TRAIN}${ANALYTIC_BATCH}_${MICRO_ROLLOUT}_${ROLLOUT}${DUP_ROLLOUT}_epo${MAX_EPOCHS}_epi${NUM_EPISODES}${HARMLOSS}${HARMLOSSREINBASELINE}${HLRBVAL}${STARTA}${ALPHA}${RTA}${RTB}${BASEACTOR_LR}_${ACTOR_LOSS}_alr${ACTOR_LR}_clr${CRITIC_LR}_${LR_SCHED}${CUSTOM_PROMPT}${SAVE_NEGDATA_THRESH}${EXPLORATION_BONUS_SA}${EXPLORATION_BONUS_BA}${BONUS_ALPHA}${COIN_FLIP_DIM}${COIN_FLIP_LR}${COIN_FLIP_BATCH}"
+PATTERN="${CURRENT_DATE}${ONLY_EVAL_NEG}_${PRETRAIN}_${REWARD_PRETRAIN}_${PROMPT_DATA}_${RM_TYPE}${BASE_PROP}${THRESH}${STARTB}${TARGET_BETA}${SEPB}${UNIW}${KL}_len${GEN_MAX_LEN}_${PARAMETERIZATION}${INITHEADBASE}${SD_DIVIDER}_batch${MICRO_TRAIN}_${TRAIN}${ANALYTIC_BATCH}_${MICRO_ROLLOUT}_${ROLLOUT}${DUP_ROLLOUT}_epo${MAX_EPOCHS}_epi${NUM_EPISODES}${HARMLOSS}${HARMLOSSREINBASELINE}${HLRBVAL}${STARTA}${ALPHA}${RTA}${RTB}${BASEACTOR_LR}_${ACTOR_LOSS}_alr${ACTOR_LR}_clr${CRITIC_LR}_${LR_SCHED}${CUSTOM_PROMPT}${SAVE_NEGDATA_THRESH}${EXPLORATION_BONUS_SA}${EXPLORATION_BONUS_BA}${BONUS_ALPHA}${COIN_FLIP_DIM}${COIN_FLIP_LR}"
 SBATCH_FILE="sbatch_${PATTERN}"
 OUTPUT_FILE="result_${PATTERN}_s1.txt"
 
--- openrlhf/cli/train_ppo.py
+++ openrlhf/cli/train_ppo.py
@@ -2272,7 +2272,6 @@ if __name__ == "__main__":
     parser.add_argument("--bonus_alpha", type=float, default=1.0, help="Scaling factor for exploration bonus: bonus = bonus_alpha * (1/sqrt(N(x)))")
     parser.add_argument("--coin_flip_dim", type=int, default=64, help="Dimension d for coin flip vectors")
     parser.add_argument("--coin_flip_lr", type=float, default=None, help="Learning rate for coin flip network (defaults to sampling_actor_lr if None)")
-    parser.add_argument("--coin_flip_batch_size", type=int, default=32, help="Batch size for coin flip network training")
 
     parser.add_argument("--do_harmlessness_training", action="store_true", help="Have an outer loop where we do harmlessness training on the base/initial model. Use --num_episodes for the inner loop/proposal/twist training steps, --harmlessness_training_num_episodes for the number of outer loop steps, and --harmlessness_training_episodes_per_loop for the number of harmlessness training steps in each loop iteration. So total harmlessness_training_num_episodes * num_episodes twist/proposal updates will be done, and harmlessness_training_num_episodes * harmlessness_training_episodes_per_loop base model updates will be done)")
     parser.add_argument("--harmlessness_training_num_episodes", type=int, default=1, help="Total number of outer loop steps (where each inner loop does --num_episodes twist/proposal updates")
