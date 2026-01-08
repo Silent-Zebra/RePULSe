b4e60b9e91da3c52a2db672fe75b8e391dd3c816
Stephen Zhao <szstephenzhao88@gmail.com>
2026-01-08 01:32:59 -0500

    differentiate saving

--- openrlhf/utils/utils.py
+++ openrlhf/utils/utils.py
@@ -239,8 +239,14 @@ def get_info_name_str(args):
     elif args.separate_reweighting_beta is not None:
         sep_beta_str = f"_sepb{args.separate_reweighting_beta}"
 
+    exploration_bonus_str = ""
+    if hasattr(args, 'exploration_bonus_sampling_actor') and args.exploration_bonus_sampling_actor is not None:
+        if args.exploration_bonus_sampling_actor == "exact_count":
+            exploration_bonus_str = "_count"
+        elif args.exploration_bonus_sampling_actor == "coin_flip":
+            exploration_bonus_str = "_cfn"
 
-    info_name_str = f"{rm_type_str}_{pretrain_str}_{reward_pretrain_str}_{prompt_data_str}_len{args.generate_max_len}_kl{args.init_kl_coef}{start_beta_str}_beta{args.target_dist_beta}{sep_beta_str}{harmlessness_train_str}{rew_trans_str}_{args.parameterization}_{args.actor_loss_type}_epo{args.max_epochs}_epi{n_episodes}{eval_str}_sch{args.lr_scheduler}_{lr_str}{critic_loss_str}{adam_betas_str}_{args.parameterization}{init_head_base_str}{sddiv_str}_s{args.seed}"
+    info_name_str = f"{rm_type_str}_{pretrain_str}_{reward_pretrain_str}_{prompt_data_str}_len{args.generate_max_len}_kl{args.init_kl_coef}{start_beta_str}_beta{args.target_dist_beta}{sep_beta_str}{harmlessness_train_str}{rew_trans_str}_{args.parameterization}_{args.actor_loss_type}_epo{args.max_epochs}_epi{n_episodes}{eval_str}_sch{args.lr_scheduler}_{lr_str}{critic_loss_str}{adam_betas_str}_{args.parameterization}{init_head_base_str}{sddiv_str}{exploration_bonus_str}_s{args.seed}"
 
     return info_name_str
 
