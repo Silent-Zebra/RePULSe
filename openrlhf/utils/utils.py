f44201a1d6ef187b5d28d8f93003fec846ff839d
Stephen Zhao <szstephenzhao88@gmail.com>
2026-01-14 22:52:34 -0500

    save coin flip -

--- openrlhf/utils/utils.py
+++ openrlhf/utils/utils.py
@@ -257,6 +257,8 @@ def get_info_name_str(args):
                 exploration_bonus_str += f"_cfus{coin_flip_update_steps}"
             if train_before:
                 exploration_bonus_str += "_before"
+            else:
+                exploration_bonus_str += "_after"
 
     info_name_str = f"{rm_type_str}_{pretrain_str}_{reward_pretrain_str}_{prompt_data_str}_len{args.generate_max_len}_kl{args.init_kl_coef}{start_beta_str}_beta{args.target_dist_beta}{sep_beta_str}{harmlessness_train_str}{rew_trans_str}_{args.parameterization}_{args.actor_loss_type}_epo{args.max_epochs}_epi{n_episodes}{eval_str}_sch{args.lr_scheduler}_{lr_str}{critic_loss_str}{adam_betas_str}_{args.parameterization}{init_head_base_str}{sddiv_str}{exploration_bonus_str}_s{args.seed}"
 
